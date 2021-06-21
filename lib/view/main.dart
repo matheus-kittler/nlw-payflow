a48d743a3bcacaa200f359d514cd4b0ebd53a6af
matheus.santos <matheus.santos@softdesign.com.br>
2021-06-21 17:34:58 -0300

    iniciando um app básico e entendendo widgets, stateless e stateful

--- lib/view/main.dart
+++ lib/view/main.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/widget/text.dart';
 
 void main() {
-  // runApp(MyApp());
-  printHelloWorld();
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class HomePage extends StatefulWidget {
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  var count = 0;
 
-  void _incrementCounter() {
+  void increment() {
+    count++;
+    //chamar o state faz buildar novamente o componente, no caso increment
     setState(() {
-      _counter++;
+
     });
   }
 
@@ -42,27 +26,15 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Contador"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: Center(child: Text("Contagens\n$count", textAlign: TextAlign.center)),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          increment();
+        },
+      ),
     );
   }
 }
